77fa8b5f67692e46d6d79b5c5df7cc983db86f8e
zhengqigo <zhengqigo@vip.163.com>
2019-01-20 10:35:29 +0800

    add create database statement

--- src/main/resources/sql/userex.sql
+++ src/main/resources/sql/userex.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `userex` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */
+CREATE DATABASE `userex` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 USE `userex`;
 
